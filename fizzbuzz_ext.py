9532b5dcca35fbb990f7993731f162633f2a4aad
Naomi T. S. <tsnaomi@users.noreply.github.com>
2014-02-15 22:09:39 -0800

    Update fizzbuzz_ext.py

--- fizzbuzz_ext.py
+++ fizzbuzz_ext.py
@@ -10,17 +10,17 @@ def FizzBuzz_Ext(num, d): # d for dictionary
     for entry in filter(lambda e: isinstance(e, int) and isinstance(d[e], str) and num % e == 0 and e != 3 and e != 5, d): # FizzBuzz is law
         verbosity += d[entry]
     if len(verbosity) == 0:
-        print num
+        return num
     else:				
-        print verbosity
+        return verbosity
 
 #TEST
 #smiles = {3: 'Fizz', 5: 'Buzz', 4: 'Crackle', 7: 'Sivv', 9: 'Pop', 'uhoh': 6}
 
 #print "\033[1m1-9:\033[0m"
 #for i in range(1, 10):
-#    FizzBuzz_Ext(i, smiles)
+#    print FizzBuzz_Ext(i, smiles)
 
 #print "\033[1m3600-3800:\033[0m" 
 #for i in range(3600, 3801): # look out for 3780
-#    FizzBuzz_Ext(i, smiles)
+#    print FizzBuzz_Ext(i, smiles)
