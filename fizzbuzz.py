2c4d0343b544d83aee8ebfc7f0e60e556ca6d3ae
Naomi T. S <tsnaomi@uw.edu>
2014-03-30 15:49:38 -0700

    Clean up

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
+
 def FizzBuzz(num):
-    ''' A method that takes one number as an argument. For multiples of three, it returns Fizz, and for multiples of five, it returns Buzz. For numbers which are multiples of both three and five, it returns FizzBuzz. In all other cases, it returns the number. '''
-    if num % (3 * 5) == 0: 
+    ''' A method that takes one number as an argument. For multiples of three,
+        it returns Fizz, and for multiples of five, it returns Buzz. For
+        numbers which are multiples of both three and five, it returns
+        FizzBuzz. In all other cases, it returns the number. '''
+    if num % (3 * 5) == 0:
         return "FizzBuzz"
     elif num % 3 == 0:
         return "Fizz"
-    elif num % 5 == 0: 
+    elif num % 5 == 0:
         return "Buzz"
     else:
         return str(num)
 
-#TEST
-#for i in range(1,101):
-#    print FizzBuzz(i)
+# TESTS
+# for i in range(1,101):
+#     print FizzBuzz(i)
